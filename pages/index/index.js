ba343764db8412644de87bdd5b3a0f2be018d74e
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-07-24 12:42:30 +0800

    首页内容完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,10 @@ Page({
   data: {
     //轮播图数组
     swiperlist: [],
+    //导航数组
+    catesList: [],
+    //楼层数据
+    floorList: [],
   },
 
   onLoad: function (options) {
@@ -19,12 +23,40 @@ Page({
     //   },
     // });
 
+    this.getSwiperList();
+    this.getCatesList();
+    this.getFloorList();
+  },
+
+  //获取轮播图
+  getSwiperList() {
     //封装写法
     request({
       url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
     }).then((result) => {
       this.setData({
         swiperlist: result.data.message,
+      });
+    });
+  },
+  //获取分类菜列表
+  getCatesList() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems",
+    }).then((result) => {
+      this.setData({
+        catesList: result.data.message,
+      });
+    });
+  },
+
+  //获取楼层数据
+  getFloorList() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/floordata",
+    }).then((result) => {
+      this.setData({
+        floorList: result.data.message,
       });
     });
   },
