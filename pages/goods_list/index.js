873bebb3e465203084c4499d77bc36da15489672
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-07-24 22:26:34 +0800

    加入购物车，收获地址完成，购物车页面完成

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,3 +1,28 @@
+/* 
+1 用户上滑页面 滚动条触底 开始加载下一页数据
+  1 找到滚动条触底事件  微信小程序官方开发文档寻找
+  2 判断还有没有下一页数据
+    1 获取到总页数  只有总条数
+      总页数 = Math.ceil(总条数 /  页容量  pagesize)
+      总页数     = Math.ceil( 23 / 10 ) = 3
+    2 获取到当前的页码  pagenum
+    3 判断一下 当前的页码是否大于等于 总页数 
+      表示 没有下一页数据
+
+  3 假如没有下一页数据 弹出一个提示
+  4 假如还有下一页数据 来加载下一页数据
+    1 当前的页码 ++
+    2 重新发送请求
+    3 数据请求回来  要对data中的数组 进行 拼接 而不是全部替换！！！
+2 下拉刷新页面
+  1 触发下拉刷新事件 需要在页面的json文件中开启一个配置项
+    找到 触发下拉刷新的事件
+  2 重置 数据 数组 
+  3 重置页码 设置为1
+  4 重新发送请求
+  5 数据请求回来 需要手动的关闭 等待效果
+
+ */
 // pages/goods_list/index.js
 import { request } from "../../request/index.js";
 import regeneratorRuntime from "../../lib/runtime/runtime";
@@ -22,6 +47,8 @@ Page({
     pagesize: 10,
   },
 
+  pageTotal: 0,
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -34,10 +61,15 @@ Page({
   //获取商品列表数据
   async getGoodsList() {
     const res = await request({ url: "/goods/search", data: this.QueryParams });
-    console.log(res.data.message);
+    console.log(res.data.message.total);
+    const total = res.data.message.total;
+    this.pageTotal = Math.ceil(total / this.QueryParams.pagesize);
     this.setData({
-      goodsList: res.data.message.goods,
+      goodsList: [...this.data.goodsList, ...res.data.message.goods],
     });
+    //console.log(this.pageTotal);
+    //下拉刷新成功后，关闭下拉刷新动画
+    wx.stopPullDownRefresh();
   },
 
   //标题点击事件
@@ -55,5 +87,30 @@ Page({
     this.setData({
       tabs,
     });
+  },
+
+  //底部上拉触发事件
+  onReachBottom() {
+    if (this.QueryParams.pagesize >= this.pageTotal) {
+      // 没有下一页数据
+      wx.showToast({ title: "没有下一页数据" });
+    } else {
+      //有下一页数据
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+
+  // 下拉刷新事件
+  onPullDownRefresh() {
+    // 1 重置数组
+    this.setData({
+      goodsList: [],
+    });
+    // 2 重置页码
+    this.QueryParams.pagenum = 1;
+    //3 发送请求
+    this.getGoodsList();
+    // console.log("下拉刷新了");
   },
 });
--- request/index.js
+++ request/index.js
@@ -1,6 +1,19 @@
+//一个页面调用多个ajax请求时，wx.showLoading要确认每个请求都有返回数据之后才会做关闭。所以计算同个页面的ajax请求的次数，并进行判断即可。ajaxTimes
+
+//同时发送异步代码次数
+let ajaxTimes = 0;
+
 //promise封装，避免回调地狱
 
 export const request = (params) => {
+  ajaxTimes++;
+  //显示加载中 效果
+  wx.showLoading({
+    title: "正在加载...", //提示的内容,
+    mask: true, //显示透明蒙层，防止触摸穿透,
+    // success: res => {}
+  });
+
   //定义公共的url
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
   return new Promise((resolve, reject) => {
@@ -12,6 +25,13 @@ export const request = (params) => {
       },
       fail: (err) => {
         reject(err);
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          //关闭正在等待的图标
+          wx.hideLoading();
+        }
       },
     });
   });
