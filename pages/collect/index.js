9e8077f7b4f52b8950eaff27fee84cda9cfb1f04
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-07-25 20:17:29 +0800

    收藏中心信息渲染完成

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -1,66 +1,42 @@
 // pages/collect/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    collect: [],
+    tabs: [
+      { id: 0, value: "商品收藏", isActive: true },
+      { id: 1, value: "品牌收藏", isActive: false },
+      { id: 2, value: "店铺收藏", isActive: false },
+      { id: 3, value: "浏览足迹", isActive: false },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onShow() {
+    const collect = wx.getStorageSync("collect") || [];
+    this.setData({
+      collect,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //标题点击事件
+  handleTabsItemChange(e) {
+    //console.log(e.detail);
+    //获取传递过来的数据
+    const { index } = e.detail;
+    //获取原数组，修改源数组
 
+    let { tabs } = this.data;
+    //遍历tabs,如果index等于数组中的index,则修改数组中的isActive
+    tabs.forEach((v, i) =>
+      i === index ? (v.isActive = true) : (v.isActive = false)
+    );
+    this.setData({
+      tabs,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
